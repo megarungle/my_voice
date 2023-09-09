286fd960aef77eced985f29a5c35fc1ee39bac96
Nikita <nikita.pasechnik199@gmail.com>
2023-09-09 16:56:33 +0300

    front end add

--- backend/src/structs.py
+++ backend/src/structs.py
@@ -1,16 +1,19 @@
 from dataclasses import dataclass
 from enum import Enum
 
+
 class Sentiment(Enum):
     negative = -1
     neutral = 0
     positive = 1
+
 
 class InferStatus(Enum):
     status_ok = 0
     status_error_init = -1
     status_error_prepare = -2
     status_error_infer = -3
+
 
 @dataclass
 class Data:
@@ -19,7 +22,7 @@ class Data:
     cluster: str = ''
     sentiment: Sentiment = None
     corrected: str = ''
-    
+
     @classmethod
     def fromJson(cls):
         return cls()
