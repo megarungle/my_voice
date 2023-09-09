a8b3b2701cdd3ac3e92f372b8e741c2f06ec7788
Nikita <nikita.pasechnik199@gmail.com>
2023-09-09 18:03:30 +0300

    front end gotovo

--- backend/src/routers/ml_models.py
+++ backend/src/routers/ml_models.py
@@ -18,15 +18,15 @@ from src.utils import parse_dataset_json
 models_router = APIRouter()
 
 
-# @models_router.post("/spellcheck")
-# def post_spellcheck(text: SpellCorrectRequest) -> str:
-#     return spelling.correct_spelling(text.input)  # FIXME: Use proper model
-#
-#
-# @models_router.post("/sentiment")
-# def post_sentiment(text: SentimentRequest) -> SentimentResult:
-#     res = sentiment.get_sentiment(text.input)
-#     return SentimentResult(label=res.label, score=res.score)
+@models_router.post("/spellcheck")
+def post_spellcheck(text: SpellCorrectRequest) -> str:
+    return spelling.correct_spelling(text.input)  # FIXME: Use proper model
+
+
+@models_router.post("/sentiment")
+def post_sentiment(text: SentimentRequest) -> SentimentResult:
+    res = sentiment.get_sentiment(text.input)
+    return SentimentResult(label=res.label, score=res.score)
 
 from collections import defaultdict
 
